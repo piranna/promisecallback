3b40d678b4d1039f529b2def71fa9906dcd559bb
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-06-18 18:28:08 +0200

    Return callback value as dictates specification

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /*
- * (C) Copyright 2014 Kurento (http://kurento.org/)
+ * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License (LGPL)
+ * version 2.1 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-2.1.html
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 
@@ -25,12 +24,12 @@ function promiseCallback(promise, callback, thisArg)
     {
       try
       {
-        callback.call(thisArg, error, result);
+        return callback.call(thisArg, error, result);
       }
       catch(exception)
       {
         // Show the exception in the console with its full stack trace
-        console.error(exception);
+        console.trace(exception);
         throw exception;
       }
     };
